b66f0422172c1b2e95cc58a3f79622bcb316ce0d
Joan Marie Verba <verba001@alumni.umn.edu>
2024-08-22 11:46:51 -0500

    comment out code

--- current.js
+++ current.js
@@ -53,7 +53,7 @@ $("#btn").on("click",locationButtonClick);
 function getWeatherData (zipCode){
   let url = "https://api.openweathermap.org/data/2.5/weather?q=" + zipCode + "&APPID=eec48f1630281ec926acbcbb20931f70";
 
-  let url = "https://api.openweathermap.org/data/2.5/weather?q=" + zipCode + "&APPID=eec48f1630281ec926acbcbb20931f70";
+  // let url = "https://api.openweathermap.org/data/2.5/weather?q=" + zipCode + "&APPID=eec48f1630281ec926acbcbb20931f70";
 
 
 
